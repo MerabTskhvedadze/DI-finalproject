36ad1495fad30599dfc1816488eba86258dfadc2
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-05-14 17:19:42 +0400

    feature(absolute imports)

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,12 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: 'components',
-        replacement: path.resolve(__dirname, 'src/components'),
-      },
-      { find: 'assets', replacement: path.resolve(__dirname, 'src/assets') },
-      { find: 'layouts', replacement: path.resolve(__dirname, 'src/layouts') },
-      { find: 'views', replacement: path.resolve(__dirname, 'src/views') },
-    ],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
+      views: path.resolve(__dirname, 'src/views'),
+    },
   },
   plugins: [react()],
 });
